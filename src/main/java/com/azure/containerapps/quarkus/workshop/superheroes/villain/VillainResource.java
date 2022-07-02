6b20a0b23cd90fdc0f4468bc2cdc1b21fdc7fa2a
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-07-02 19:50:46 +0200

    refactor

--- src/main/java/com/azure/containerapps/quarkus/workshop/superheroes/villain/VillainResource.java
+++ src/main/java/com/azure/containerapps/quarkus/workshop/superheroes/villain/VillainResource.java
@@ -20,6 +20,7 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 @Path("/api/villains")
 @Tag(name = "villains")
 @ApplicationScoped
+@Produces(MediaType.APPLICATION_JSON)
 public class VillainResource {
 
     Logger logger;
@@ -29,24 +30,25 @@ public class VillainResource {
         this.logger = logger;
     }
 
-    @Operation(summary = "Returns a random villain")
     @GET
     @Path("/random")
+    @Operation(summary = "Returns a random villain")
     @APIResponse(
         responseCode = "200",
         content = @Content(mediaType = APPLICATION_JSON, schema = @Schema(implementation = Villain.class, required = true))
     )
     public Response getRandomVillain() {
-        logger.info("Getting a Random Villain");
+        logger.info("Getting a random villain");
         Villain villain = Villain.findRandom();
-        logger.debug("Found random villain " + villain);
+        logger.debug("Found random villain : " + villain);
         return Response.ok(villain).build();
     }
 
-    @Operation(summary = "Returns hello from the Villain Resource")
-    @Path("/hello")
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
+    @Tag(name = "hello")
+    @Operation(summary = "Returns hello from the villain resource")
     public String hello() {
         logger.info("Hello from Villain Resource");
         return "Hello from Villain Resource";
